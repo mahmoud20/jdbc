b02cbbd7569691b3ce42905cc114f82a36c25eb2
mahmoud20 <mahmoud_hafez20@yahoo.com>
2017-06-18 10:35:59 +0300

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,19 @@
+ // document.write("<p> My Name Is Mahmoud </p>");
+// var
+// x=9;
+// y=8;
+// z=x+y;
+// document.write(z);
+
+
+var x, y, z, d;
+x = 500;
+y = 50;
+z = 100;
+d = 250;
+document.getElementById("price").innerHTML = x;
+document.getElementById("p1").innerHTML = x - y;
+document.getElementById("p2").innerHTML = x - z;
+document.getElementById("p3").innerHTML = x - d;
+
+
